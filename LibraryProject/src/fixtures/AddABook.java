42c734abdd6a77f134fff54fd22fce0e9d465179
glenmccarthy <gb.mccarthy@gmail.com>
2014-08-08 23:40:02 +0100

    Adding first fitnesse test and fixture

---
+++ LibraryProject/src/fixtures/AddABook.java
@@ -0,0 +1,18 @@
+package fixtures;
+
+public class AddABook {
+
+	
+	public void GivenTheNumberOfBooksInTheLibraryIs(String numberOfBooks){
+		
+	}
+	
+	public void WhenIAddABook(){
+		
+	}
+	
+	public int theNumberOfBooksInTheLibraryIs(){
+		return 0;
+		
+	}
+}
